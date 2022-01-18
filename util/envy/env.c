20ee9326bcb523a96ce66f3dc12554055da26c32
c8p2d01 <c8p2d01@gmail.com>
2022-01-18 14:16:49 +0100

    first implementation of simple environment, still beta tho.fixes #20

---
+++ util/envy/env.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cdahlhof <cdahlhof@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/14 17:53:05 by cdahlhof          #+#    #+#             */
+/*   Updated: 2022/01/18 14:08:45 by cdahlhof         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "env.h"
+
+/*
+	Overview in this File:
+		create the execution of the commands "env"
+	--env--
+		display the environmental variables, including those changes by "export" and "unset"
+		only display if a <key>=<value> structure is apparent
+		if given an argument, see it as path from where to display its environment, (mostly "<input> : Permission deined")
+		however if the argument has an '=' symbol, print it at the end of the environment, but don't add it
+		mutiple arguments count, as long as they declare <key>=<value> no naming specifications (all ascii possible)
+		env does not interact with input files
+*/
+
+void	disp_env(t_shell *s)
+{
+	int	i;
+
+	i = 0;
+	while (s->env[i])
+	{
+		if (ft_strchr(s->env[i], '='))
+			printf("%i\t%s\n", i, s->env[i]);
+		i++;
+	}
+}
+
+void	ms_env(t_seq *q, t_shell *s)
+{
+	int	i;
+
+	i = 0;
+	while (q->cmd.cmd_args[i])
+	{
+		if (!ft_strchr(q->cmd.cmd_args[i], '='))
+		{
+			printf("env : %s: No such file or directory\n", q->cmd.cmd_args[i]);
+			return ;
+		}
+		if (q->cmd.cmd_args[i][0] == '=')
+		{
+			printf("env : setenv: %s: Invalid argument\n", q->cmd.cmd_args[i]);
+			return ;
+		}
+		i++;
+	}
+	i = 0;
+	while (s->env[i])
+	{
+		if (ft_strchr(s->env[i], '='))
+			printf("%s\n", s->env[i]);
+		i++;
+	}
+	i = 0;
+	while (q->cmd.cmd_args[i])
+	{
+		printf("%s\n", q->cmd.cmd_args[i]);
+		i++;
+	}
+}
+
+//	a function to extract the value of an environmental variable
+char	*getenv(char *key, t_shell *s)
+{
+	int	p;
+
+	p = keyfinder(key, ft_strlen(key), s->env);
+	if (p >= 0)
+		return (s->env[p])
+	else
+		return (NULL);
+}
+
+// int	main(int argc, char **argv, char **env)
+// {
+// 	t_shell	s;
+
+// 	if (argc != 1 || argv[1])
+// 		return(2);
+// 	create_env(env, &s);
+// 	// disp_env(&s);
+
+// 	t_seq	*q = ft_calloc(1, sizeof(t_seq));
+
+// 	//	test the functionality of env
+// 	q->cmd.cmd_args = ft_split("lal= me= =", ' ');
+// 	ms_env(q, &s);
+// 	free(q->cmd.cmd_args);
+// 	// q->cmd.cmd_args = ft_split("#", ' ');
+// 	// ms_export(q, &s);
+	
+// 	free(q);
+// 	return(0);
+// }
---
+++ util/envy/utils.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cdahlhof <cdahlhof@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/14 17:54:43 by cdahlhof          #+#    #+#             */
+/*   Updated: 2022/01/17 11:35:31 by cdahlhof         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "env.h"
+
+//	duplicate the system-environment and store the copy in the given shell
+int	create_env(char **sysenv, t_shell *s)
+{
+	int		i;
+	int		l;
+
+	l = 0;
+	while (sysenv[l])
+		l++;
+	s->env = ft_calloc((l + 1), sizeof(char *));
+	if (!s->env)
+		return (1);
+	i = 0;
+	while (i < l)
+	{
+		s->env[i] = ft_strdup(sysenv[i]);
+		if (!s->env[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+//	return the number of lines in a list
+size_t	outerlen(char **list)
+{
+	int i;
+
+	i = 0;
+	while(list && list[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+//	search the key in a list of key;value and return the line number (start 0)
+int	keyfinder(char *key, int keylen, char **list)
+{
+	int	i;
+
+	i = 0;
+	while (i < outerlen(list))
+	{
+		if ((ft_strncmp(key, list[i], keylen) == 0) && \
+			(!list[i][keylen] || list[i][keylen] == '='))
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+//	check if the givien variable key complys to the char norms
+int	key_error(char *input)
+{
+	int	i;
+
+	i = 0;
+	if (!ft_isalpha(input[0]) && input[i] != '_')
+		return (1);
+	while (input[i] && input[i] != '=')
+	{
+		if (!ft_isalnum(input[i]) && input[i] != '_')
+			return (1);
+		i++;
+	}
+	return (0);
+}
