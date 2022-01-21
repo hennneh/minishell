87951d17f2e7b3d21a544a22d53a21253e47926c
hennneh <hendrik.lehmann96@gmail.com>
2022-01-21 13:55:38 +0100

    finding user for prompt

--- util/henne/prompt.c
+++ util/henne/prompt.c
@@ -6,7 +6,7 @@
 /*   By: hlehmann <hlehmann@student.42wolfsburg.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 07:53:13 by hlehmann          #+#    #+#             */
-/*   Updated: 2022/01/21 09:22:49 by hlehmann         ###   ########.fr       */
+/*   Updated: 2022/01/21 13:55:23 by hlehmann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,30 @@
 
 char *get_user(t_shell *shell)
 {
+	int		i;
+	int		j;
+	int		k;
+	char	*user;
 
+	i = 0;
+	j = 5;
+	k = 0;
+	while (shell->env[i])
+	{
+		if (find_user(shell->env[i]))
+		{
+			user = malloc(sizeof(char) * ft_strlen(shell->env[i] - 4));
+			while (shell->env[i][j])
+			{
+				user[k] = shell->env[i][j];
+				k++;
+				j++;
+			}
+			return (user);
+		}
+		i++;
+	}
+	return (NULL);
 }
 
 char *get_last_dir(t_shell *shell)
