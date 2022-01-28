ff6a6fc1d3e90639caf52961d2ee5cfc5bbfd28f
hennneh <hendrik.lehmann96@gmail.com>
2022-01-28 13:20:09 +0100

    prompt has colors

--- util/henne/prompt.c
+++ util/henne/prompt.c
@@ -6,26 +6,39 @@
 /*   By: hlehmann <hlehmann@student.42wolfsburg.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 07:53:13 by hlehmann          #+#    #+#             */
-/*   Updated: 2022/01/28 10:21:14 by hlehmann         ###   ########.fr       */
+/*   Updated: 2022/01/28 13:19:49 by hlehmann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
 
-char	*prompt(t_shell *s)
+char	*prompt(void)
 {
 	char	*prompt;
-	char	dir;
+	char	*dir;
 	char	*check;
 
 	dir = malloc(sizeof(char) * 1024);
 	check = NULL;
-	check = getcwd(dir, 1024);
+	check = getcwd(dir, 1124);
 	if (!check)
 		return (NULL);
 	check = ft_strrchr(dir, '/');
 	check++;
-	prompt = ft_strjoin("minishell: ", check);
+	prompt = ft_strjoin("\033[1;32mminishell: \033[0m", check);
 	free(dir);
-	return (prompt);
+	dir = ft_strjoin(prompt, " $ ");
+	free(prompt);
+	return (dir);
 }
+/*
+int	main(int argc, char **argv, char **env)
+{
+	if (argc!= 1 && strncmp(argv[0], "./minishell", strlen(argv[0])))
+		write(1, "\n", 1);
+	char *c = prompt();
+	printf("%s\n", c);
+	free(c);
+	return (0);
+}
+*/
